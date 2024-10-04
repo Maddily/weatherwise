b40c4048df64ce1b359baaca894b2627c68248d9
Maddie <115709272+Maddily@users.noreply.github.com>
2024-10-04 12:00:56 +0300

    Add _fetchData method to AppController
    - Implement method to fetch weather data from
      Visual Crossing's API
    - Check rate limit and throw error if limit exceeded
    - Construct API URL with location and API key
    - Validate API response status and handle errors
    - Update rate limit cache after successful request
    - Cache fetched weather data with today's date
      as part of the key

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -23,6 +23,45 @@ export default class AppController {
   }
 
   /**
+   * Fetch weather data from the Visual Crossing's API
+   * @param {string} location - The name of a city
+   * @param {number} lifetime - The lifetime of the data in the cache, in milliseconds
+   * @returns The fetched weather data
+   */
+  static async _fetchData(ipAddress, location, lifetime) {
+    // Check if the user has made 20 requests today and throw an error if so.
+    if (await AppController._checkRateLimit(ipAddress)) {
+      throw new Error('Too many requests! Try again tomorrow.');
+    }
+
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${AppController._APIKEY}`;
+
+    const response = await fetch(url);
+
+    // In case the status code isn't in the 200 range, throw an error
+    AppController._checkResponseStatus(response, location);
+
+    // Update the cache value for rate limiting
+    AppController._rateLimit(ipAddress);
+
+    const weatherData = await response.json();
+
+    /**
+     * Cache the fetched data.
+     * Today's date is used as a part of the key string.
+     */
+    const today = weatherData.days[0].datetime;
+
+    redisClient.set(
+      `${location}-${today}`,
+      JSON.stringify(weatherData),
+      lifetime
+    );
+
+    return weatherData;
+  }
+
+  /**
    * Throw an error if the status code of the response
    * is not in the 200 range.
    * @param {Promise} response - The response of fetching data from the API
