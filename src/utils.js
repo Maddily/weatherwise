796b03032a248fb7ca3af57f2223a3d903eecc3e
Maddie <115709272+Maddily@users.noreply.github.com>
2024-10-05 11:38:27 +0300

    Add formatTime function
    Implement function to format time strings
    from 'HH:mm:ss' to 'h a'

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { parse, format } from 'date-fns';
+
 /**
  * Determine the wind direction based on the given degrees.
  * @param {number} degrees - Wind direction degrees
@@ -25,4 +27,15 @@ export function getWindDirectionText(degrees) {
 
   const index = Math.round(degrees / 22.5) % 16;
   return directions[index];
+}
+
+/**
+ * Format a given time string
+ * @param {string} timeString - EX: 12:00:00
+ * @returns The time string formatted. EX: 12 AM
+ */
+export function formatTime(timeString) {
+  const date = parse(timeString, 'HH:mm:ss', new Date());
+
+  return format(date, 'h a');
 }
