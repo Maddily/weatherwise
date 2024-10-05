7361926d01d01a788a5ad48100ea1340ffdc9666
Maddie <115709272+Maddily@users.noreply.github.com>
2024-10-05 12:28:08 +0300

    Add createHourlyIcon function to icons.js
    Implement function to create and return
    an SVG icon for hourly weather

--- src/icons.js
+++ src/icons.js
@@ -119,3 +119,23 @@ export function createLocationIcon() {
 
   return svg;
 }
+
+// Create and return an svg icon for hourly forecast heading
+export function createHourlyIcon() {
+  const xmlns = 'http://www.w3.org/2000/svg';
+
+  const svg = document.createElementNS(xmlns, 'svg');
+  svg.setAttribute('xmlns', xmlns);
+  svg.setAttribute('viewBox', '0 0 24 24');
+  svg.setAttribute('fill', '#fff1d4');
+
+  const path = document.createElementNS(xmlns, 'path');
+  path.setAttribute(
+    'd',
+    'M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12S17.5 2 12 2M17 13H11V7H12.5V11.5H17V13Z'
+  );
+
+  svg.appendChild(path);
+
+  return svg;
+}
