02cd17058af6a3b6714b264685d5c59e8cc2aa72
Maddie <115709272+Maddily@users.noreply.github.com>
2024-10-05 12:31:14 +0300

    Add createVisibilityIcon function to icons.js
    Implement function to create and return
    an SVG icon for visibility

--- src/icons.js
+++ src/icons.js
@@ -220,3 +220,23 @@ export function createUvIndexIcon() {
 
   return svg;
 }
+
+// Create and return an svg icon for visibility
+export function createVisibilityIcon() {
+  const xmlns = 'http://www.w3.org/2000/svg';
+
+  const svg = document.createElementNS(xmlns, 'svg');
+  svg.setAttribute('xmlns', xmlns);
+  svg.setAttribute('viewBox', '0 0 24 24');
+  svg.setAttribute('fill', '#fff1d4');
+
+  const path = document.createElementNS(xmlns, 'path');
+  path.setAttribute(
+    'd',
+    'M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z'
+  );
+
+  svg.appendChild(path);
+
+  return svg;
+}
