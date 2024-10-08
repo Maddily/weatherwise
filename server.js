a11714cb0cb460cf70c9af0cc63f4af64fe1a4b9
Maddie <115709272+Maddily@users.noreply.github.com>
2024-10-08 11:10:45 +0300

    Configure server to serve static files from dist

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import createRouter from './routes/index.js';
 import AppController from './controllers/AppController.js';
 
 const app = express();
 const port = 5000;
 const router = createRouter(AppController);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from /dist
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(cors());
 app.use(router);
