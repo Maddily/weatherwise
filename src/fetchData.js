feaac5e6ceb1d47961166e86f4e9a5dc8070bf25
Maddie <115709272+Maddily@users.noreply.github.com>
2024-10-09 09:47:48 +0300

    Use the deployment URL directly in `fetch`
    The server wasn't working on Vercel even if
    the URL environment variable was set. It seems
    hard-coding the deployment URL in the `fetch`
    call is the only way to make it work.

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,5 +1,3 @@
-const URL = process.env.URL || 'http://localhost:5000';
-
 /**
  * Fetch data from the backend API
  * @param {string} type - today, daily or hourly
@@ -7,7 +5,7 @@ const URL = process.env.URL || 'http://localhost:5000';
  * @returns The weather data fetched from the backend API
  */
 export default async function fetchData(type, location) {
-  const response = await fetch(`${URL}/${type}/${location}`);
+  const response = await fetch(`https://weatherwiseforecast.vercel.app/${type}/${location}`);
 
   if (!response.ok) {
     const { error } = await response.json();
